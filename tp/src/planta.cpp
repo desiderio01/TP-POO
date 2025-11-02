bc033a36f1b615cfbcd3b9c23e3c7fcad432d810
desiderio01 <davidcvs08@gmail.com>
2025-11-02 12:02:16 +0000

    alteracoes

--- tp/src/planta.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by david on 01/11/2025.
-//
-
-#include "../include/planta.h"
-
-Planta::Planta(int agua_inicial, int nut_inicial)
-    : agua_interna(agua_inicial), nutrientes_interna(nut_inicial), idade(0) {}
